f9b230ec01630da0b5d84fea83f05f380c4d6395
Christopher David Desjardins <cddesjardins@gmail.com>
2018-01-09 15:00:04 -0800

    updated probability plot

--- R/prob_plot.R
+++ R/prob_plot.R
@@ -1,11 +1,12 @@
 #' Makes univariate probability plots
 #' @param x is the independent variable
-#' @param lab is the x-axis label
+#' @param xlab is the x-axis label
+#' @param ylab is the x-axis label
 #' @param outcome the dependent variable
-prob_plot <- function(x, outcome, lab){
+prob_plot <- function(x, outcome, xlab){
   resp <- seq(min(x, na.rm = T), max(x, na.rm = T), by = .1)
   pred <- exp(coef(mod0)[[1]] + coef(mod0)[[2]]*resp) / (1 + exp(coef(mod0)[[1]] + coef(mod0)[[2]]*resp))
   obs_points <- aggregate(outcome, by = list(round(x)), mean, na.rm = T)
-  plot(pred ~ resp, type = "l", xlab = lab, ylab = "Probability of transitioning to an unhealthier weight", ylim = c(min(obs_points$x, pred), max(obs_points$x, pred)))
+  plot(pred ~ resp, type = "l", xlab = xlab, ylab = ylab, ylim = c(min(obs_points$x, pred), max(obs_points$x, pred)))
   points(x = obs_points$Group.1, y = obs_points$x)
 }
